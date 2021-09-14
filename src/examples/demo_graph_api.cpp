b0f1135d79d4ef2528edc7dc5df0bf6cc14ff573
Viet Anh Nguyen <vietanh.dev@gmail.com>
2021-09-14 19:32:21 +0700

    Refactor and add license

--- src/examples/demo_graph_api.cpp
+++ src/examples/demo_graph_api.cpp
@@ -1,17 +1,31 @@
+// Copyright 2021 The DaisyKit Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "daisykitsdk/graphs/core/graph.h"
+#include "daisykitsdk/graphs/core/node.h"
+#include "daisykitsdk/graphs/core/utils.h"
+#include "daisykitsdk/graphs/img_proc/binarize_node.h"
+#include "daisykitsdk/graphs/img_proc/grayscale_node.h"
+#include "daisykitsdk/graphs/img_proc/img_viz_node.h"
+#include "daisykitsdk/thirdparties/json.hpp"
+
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <streambuf>
 #include <string>
-
-#include <daisykitsdk/graphs/core/graph.h>
-#include <daisykitsdk/graphs/core/node.h>
-#include <daisykitsdk/graphs/core/utils.h>
-#include <daisykitsdk/graphs/img_proc/binarize_node.h>
-#include <daisykitsdk/graphs/img_proc/grayscale_node.h>
-#include <daisykitsdk/graphs/img_proc/img_viz_node.h>
-#include <daisykitsdk/thirdparties/json.hpp>
 
 using namespace cv;
 using namespace std;
