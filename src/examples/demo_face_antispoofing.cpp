bb6705fffb38140e15110b750aa260e93eaa088e
DuongNg2911 <127082369+DuongNg2911@users.noreply.github.com>
2023-03-15 18:28:33 +0700

    Update demo_face_antispoofing.cpp file

--- src/examples/demo_face_antispoofing.cpp
+++ src/examples/demo_face_antispoofing.cpp
@@ -48,9 +48,9 @@ int main(int, char**) {
 
   for (size_t i = 0; i < face_anti_spoof.size(); ++i) {
     cv::rectangle(draw,
-                  cv::Rect(face_anti_spoof[i].x1, face_anti_spoof[i].y1, face_anti_spoof[i].x2, face_anti_spoof[i].y2),
+                  cv::Rect(face_anti_spoof[i].x, face_anti_spoof[i].y, face_anti_spoof[i].w, face_anti_spoof[i].h),
                   cv::Scalar(0, 255, 0), 2);
-    string result = face_anti_spoof[i].confidence >= 0.5 ? "Real" : "Spoof";
+    string result = face_anti_spoof[i].real_fake_score >= 0.5 ? "Real" : "Spoof";
     cout << result << endl;
   }
   imshow("Image", draw);
