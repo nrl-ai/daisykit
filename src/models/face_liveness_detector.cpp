bee8b7071f671b0508cd749ad98e8f2999f04be0
duongng2911 <duongng2911@gmail.com>
2023-03-28 21:06:01 +0700

    Update model

--- src/models/face_liveness_detector.cpp
+++ src/models/face_liveness_detector.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "daisykit/models/liveness_detector.h"
+#include "daisykit/models/face_liveness_detector.h"
 #include "daisykit/common/types/face_extended.h"
 #include "daisykit/processors/image_processors/img_utils.h"
 
